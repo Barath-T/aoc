af77c040d592bfd19683570a9126d43e730e52b6
Barath T <barath1272003@gmail.com>
2024-03-07 07:35:37 +0530

    draft:2023 day9 1

--- working/main.cpp
+++ working/main.cpp
@@ -1,52 +1,53 @@
 #include <iostream>
 #include <regex>
-#include <string>
-#include <unordered_map>
+#include <vector>
+
+int find_next(std::vector<int> seq);
 
 int main(int argc, char **argv) {
 
-  std::string path;
-  getline(std::cin, path);
+  std::vector<std::vector<int>> input;
+  std::string line;
 
-  std::unordered_map<std::string, std::pair<std::string, std::string>> network;
-
-  std::string temp;
-  getline(std::cin, temp);
-  temp = "";
-
-  std::regex regex_network(R"((\w+) = \((\w+), (\w+)\))");
-
-  while (getline(std::cin, temp)) {
-    std::smatch match;
-    std::regex_match(temp, match, regex_network);
-    network[match[1]] = std::make_pair(match[2], match[3]);
+  while (getline(std::cin, line)) {
+    input.push_back(std::vector<int>());
+    std::string temp = line;
+    std::regex digit_regex(R"(\d+)");
+    for (std::smatch match; std::regex_search(temp, match, digit_regex);) {
+      input[input.size() - 1].push_back(std::stoi(match[0]));
+      temp = match.suffix();
+    }
   }
 
-  uint step = 0;
-  bool found = false;
+  int result = 0;
 
-  std::string curr = "AAA";
-  std::string next;
-  for (int i = 0; !found; i++) {
-    if (i == path.size()) {
-      i = 0;
-    }
-    switch (path[i]) {
-    case 'R':
-      next = network[curr].second;
-      break;
-    case 'L':
-      next = network[curr].first;
-      break;
-    }
-    step++;
-    if (next == "ZZZ") {
-      found = true;
-      continue;
-    }
-    curr = next;
+  for (int i = 0; i < input.size(); i++) {
+    result += find_next(input[i]);
   }
-
-  std::cout << step << std::endl;
+  std::cout << result << std::endl;
   return 0;
+}
+
+int find_next(std::vector<int> seq) {
+  int result;
+  std::vector<int> next_sequence;
+  bool all_zeros = true;
+  for (int i = 0; i < seq.size() - 1; i++) {
+    int diff = seq[i + 1] - seq[i];
+    std::cout << diff << " ";
+    next_sequence.push_back(diff);
+    if (all_zeros && (diff != 0)) {
+      all_zeros = false;
+    }
+  }
+  std::cout << std::endl;
+
+  if (all_zeros)
+    return 0;
+
+  int down = find_next(next_sequence);
+  result = seq[seq.size() - 1] + down;
+  std::cout << seq[seq.size() - 1] << "   " << down << std::endl;
+  std::cout << "   " << result << std::endl;
+  return result;
 }
